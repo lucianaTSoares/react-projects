ef66e4c875691e10202d430a617528e9133f674b
Luciana Soares <luciana.soares@6place.com.br>
2022-06-24 17:25:40 -0300

    useEffect

--- react_timer/src/components/Stopwatch/index.tsx
+++ react_timer/src/components/Stopwatch/index.tsx
@@ -1,10 +1,26 @@
 import Button from "../Button";
 import Clock from "./Clock";
 import style from "./style.module.scss";
-import timeConverter from '../../common/utils/timeConverter'
+import timeConverter from "../../common/utils/timeConverter";
+import TypeTask from "../../types/typeTasks";
+import { useEffect, useState } from "react";
 
-export default function Stopwatch() {
-  console.log(timeConverter('01:00:00'))
+interface StopwatchProps {
+  selected: TypeTask | undefined;
+}
+
+export default function Stopwatch({ selected }: StopwatchProps) {
+  const [time, setTime] = useState<number>();
+
+  // o useEffect "observa" a mudança do estado e, sempre que ele mudar, executa uma função fazendo alguma alteração.
+  // se comporta como o "observable"
+  useEffect(() => {
+    // mesma coisa que "if(selecionado && selecionado.tempo)"
+    if (selected?.time) {
+      setTime(timeConverter(selected.time));
+    }
+  },[selected]);
+
   return (
     <div className={style.stopwatch}>
       <p className={style.title}>Escolha um card e inicie a cronometragem</p>
